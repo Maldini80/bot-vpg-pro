a42f871b9c0be101a89fcae2bfe5183c1f394e25
Maldini80 <dbzmaldini@gmail.com>
2025-07-19 19:45:25 +0200

    team.js

--- src/models/team.js
+++ src/models/team.js
@@ -5,12 +5,9 @@ const teamSchema = new mongoose.Schema({
     guildId: { type: String, required: true },
     league: { type: String, required: true },
     logoUrl: { type: String, required: true },
-    
-    managerId: { type: String, unique: true, sparse: true }, // sparse permite multiples 'null'
+    managerId: { type: String, unique: true, sparse: true },
     captains: [{ type: String }],
     players: [{ type: String }],
-
-    // Guardamos los datos del webhook para cada equipo
     webhookId: { type: String },
     webhookToken: { type: String },
 });
