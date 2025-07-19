c7f968746bd2f11ed463876ffa36ad16c1b3eeba
Maldini80 <dbzmaldini@gmail.com>
2025-07-20 01:24:47 +0200

    Delete src/commands/teamChatChannel.js

--- src/commands/teamChatChannel.js
+++
@@ -1,9 +0,0 @@
-// src/models/teamChatChannel.js
-const mongoose = require('mongoose');
-
-const teamChatChannelSchema = new mongoose.Schema({
-    channelId: { type: String, required: true, unique: true },
-    guildId: { type: String, required: true },
-});
-
-module.exports = mongoose.model('TeamChatChannel', teamChatChannelSchema);
