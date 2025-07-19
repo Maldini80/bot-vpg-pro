5b78dad76fb4a35b0bb4f267b23761692a65f114
Maldini80 <dbzmaldini@gmail.com>
2025-07-19 18:20:19 +0200

    config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,16 +1,5 @@
-// Este archivo guardará IDs importantes para que no estén esparcidos por el código.
-// Iremos añadiendo más cosas aquí a medida que construyamos los módulos.
-
 module.exports = {
-    // ID del rol que pueden aprobar/rechazar solicitudes (Árbitros/Admins)
-    // Asegúrate de que esta variable de entorno esté en Render.
-    ROL_APROBADOR_ID: process.env.APPROVER_ROLE_ID, // Ejemplo: '1393505777443930183'
-
-    // ID del canal donde los usuarios inician la solicitud de mánager.
+    ROL_APROBADOR_ID: process.env.APPROVER_ROLE_ID,
     CANAL_SOLICITUDES_ID: process.env.REQUEST_CHANNEL_ID,
-
-    // ID del canal privado donde los admins/árbitros ven las solicitudes.
     CANAL_APROBACIONES_ID: process.env.APPROVAL_CHANNEL_ID
 };
-
-module.exports = { getVpgProfile };
