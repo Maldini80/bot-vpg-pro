f397334713fd550012f554b41661a6cd14bc1477
Maldini80 <dbzmaldini@gmail.com>
2025-07-19 11:54:24 +0200

    deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,8 +3,8 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
-// Esta es la línea que hemos corregido
-const commandsPath = path.join(__dirname, 'commands');
+// Esta es la línea que corregimos para que apunte a 'src/commands'
+const commandsPath = path.join(__dirname, 'src', 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
@@ -18,12 +18,10 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 (async () => {
     try {
         console.log(`Refrescando ${commands.length} comandos de aplicación (/).`);
-
         const data = await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands },
         );
-
         console.log(`¡Éxito! Se han recargado ${data.length} comandos.`);
     } catch (error) {
         console.error(error);
