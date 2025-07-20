f11955c87740037b951a4730467b315e2a0b31ef
Maldini80 <dbzmaldini@gmail.com>
2025-07-20 02:22:49 +0200

    team.js

--- src/models/team.js
+++ src/models/team.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 
 const teamSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
+    abbreviation: { type: String, required: true }, // <-- CAMPO NUEVO
     guildId: { type: String, required: true },
     league: { type: String, required: true },
     logoUrl: { type: String, required: true },
     managerId: { type: String, unique: true, sparse: true },
     captains: [{ type: String }],
     players: [{ type: String }],
-    // Los campos webhookId y webhookToken han sido eliminados.
 });
 
 module.exports = mongoose.model('Team', teamSchema, 'teams');
