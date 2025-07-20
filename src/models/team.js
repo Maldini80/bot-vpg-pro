6e6bf106fe98ae3dd2c99681f4db7dfe78ab7b97
Maldini80 <dbzmaldini@gmail.com>
2025-07-20 09:31:37 +0200

    team.js

--- src/models/team.js
+++ src/models/team.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 
 const teamSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    abbreviation: { type: String, required: true }, // <-- CAMPO NUEVO
+    abbreviation: { type: String, required: true },
     guildId: { type: String, required: true },
     league: { type: String, required: true },
     logoUrl: { type: String, required: true },
     managerId: { type: String, unique: true, sparse: true },
     captains: [{ type: String }],
     players: [{ type: String }],
+    recruitmentOpen: { type: Boolean, default: true } // <-- NUEVO CAMPO
 });
 
 module.exports = mongoose.model('Team', teamSchema, 'teams');
