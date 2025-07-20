843b400cc63c1fcdad6f079799605366b6d2b6d1
Maldini80 <dbzmaldini@gmail.com>
2025-07-20 09:33:00 +0200

    panel-solicitud.js

--- src/commands/panel-solicitud.js
+++ src/commands/panel-solicitud.js
@@ -11,26 +11,13 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setTitle('Centro de Control de Jugador VPG')
             .setDescription('Aquí puedes interactuar con el sistema de equipos.')
-            .addFields(
-                { name: '📝 Registrar Equipo', value: 'Si eres un nuevo mánager, pulsa aquí para iniciar el proceso de registro de tu equipo.' },
-                { name: '👥 Ver Equipos', value: 'Explora la lista de equipos registrados y consulta sus plantillas.' },
-                { name: '🚪 Abandonar Equipo', value: 'Si ya no quieres pertenecer a tu equipo actual, puedes abandonarlo aquí.' }
-            )
             .setColor('#3498db');
 
         const row = new ActionRowBuilder().addComponents(
-            new ButtonBuilder()
-                .setCustomId('request_manager_role_button')
-                .setLabel('Registrar Equipo')
-                .setStyle(ButtonStyle.Success),
-            new ButtonBuilder()
-                .setCustomId('view_teams_button')
-                .setLabel('Ver Equipos')
-                .setStyle(ButtonStyle.Primary),
-            new ButtonBuilder()
-                .setCustomId('leave_team_button')
-                .setLabel('Abandonar Equipo')
-                .setStyle(ButtonStyle.Danger)
+            new ButtonBuilder().setCustomId('request_manager_role_button').setLabel('📝 Registrar Equipo').setStyle(ButtonStyle.Success),
+            new ButtonBuilder().setCustomId('view_teams_button').setLabel('👥 Ver Equipos').setStyle(ButtonStyle.Primary),
+            new ButtonBuilder().setCustomId('leave_team_button').setLabel('🚪 Abandonar Equipo').setStyle(ButtonStyle.Danger),
+            new ButtonBuilder().setCustomId('apply_to_team_button').setLabel('✉️ Aplicar a un Equipo').setStyle(ButtonStyle.Secondary)
         );
         await interaction.channel.send({ embeds: [embed], components: [row] });
         await interaction.reply({ content: 'Panel de usuario creado con éxito.', ephemeral: true });
