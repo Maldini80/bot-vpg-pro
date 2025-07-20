e8a761c3014ff0388ddccd795262f4099d7bb907
Maldini80 <dbzmaldini@gmail.com>
2025-07-20 11:40:49 +0200

    autocompleteHandler.js

---
+++ src/handlers/autocompleteHandler.js
@@ -0,0 +1,10 @@
+// src/handlers/autocompleteHandler.js
+const Team = require('../models/team.js');
+
+module.exports = async (client, interaction) => {
+    // Este es el manejador para las interacciones de autocompletado.
+    // Actualmente no tenemos comandos activos que usen autocompletado,
+    // pero mantenemos la estructura para futuras implementaciones.
+
+    /*
+    EJEMPLO DE USO FUT
