3ad5cdf2b34de97685237083c73921f5f073c524
Maldini80 <dbzmaldini@gmail.com>
2025-08-06 20:48:15 +0200

    user.js

--- src/models/user.js
+++ src/models/user.js
@@ -4,10 +4,13 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     discordId: { type: String, required: true, unique: true },
     vpgUsername: { type: String, default: null },
-    // NUEVOS CAMPOS PARA LAS POSICIONES
     primaryPosition: { type: String, default: null },
     secondaryPosition: { type: String, default: null },
     twitterHandle: { type: String, default: null },
+    // --- NUEVOS CAMPOS AÑADIDOS ---
+    psnId: { type: String, default: null },
+    eaId: { type: String, default: null },
+    // --- FIN DE NUEVOS CAMPOS ---
     teamName: { type: String, default: null },
     teamLogoUrl: { type: String, default: null },
     isManager: { type: Boolean, default: false },
