e2fef411a8dbf7a2f2294c92bb65b336d4aec62c
Maldini80 <dbzmaldini@gmail.com>
2025-08-06 08:38:07 +0200

    user.js

--- src/models/user.js
+++ src/models/user.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     discordId: { type: String, required: true, unique: true },
-    vpgUsername: { type: String, required: true },
-    position: { type: String, default: null }, // <-- CAMPO NUEVO
+    // CAMBIO: Hacemos que no sea requerido para que el perfil pueda crearse primero
+    vpgUsername: { type: String, default: null },
+    position: { type: String, default: null },
+    // NUEVO: Campo para el Twitter del usuario
+    twitterHandle: { type: String, default: null },
     teamName: { type: String, default: null },
     teamLogoUrl: { type: String, default: null },
     isManager: { type: Boolean, default: false },
